ababe4685768ee02d494951e51cfb3b6aec23bd9
etaillo <elizabeth_taillon@optum.com>
2020-01-29 17:05:21 -0500

    Initial Commit

---
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package clspkg;
+
+/**
+ * @author L00156766
+ *
+ */
+public class Greetings {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Greetings LYIT student!");
+
+	}
+
+}
